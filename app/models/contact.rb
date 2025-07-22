bb92819e3e8ce5bc3b05b667b726271ab2098972
Vítor Gabriel <108680269+vggd18@users.noreply.github.com>
2025-07-22 21:08:13 +0000

    feat: contact model + crud

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,7 @@
 class Contact < ApplicationRecord
+    validates :name, presence: true, length: { maximum: 100 }
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :subject, presence: true, length: { maximum: 200 }
+    validates :message, presence: true
+    scope :recent, -> { order(created_at: :desc) }
 end
--- db/migrate/20250719201401_create_contacts.rb
+++ db/migrate/20250719201401_create_contacts.rb
@@ -1,12 +1,15 @@
 class CreateContacts < ActiveRecord::Migration[8.0]
   def change
     create_table :contacts do |t|
-      t.string :name
-      t.string :email
-      t.string :subject
-      t.text :message
+      t.string :name, null: false
+      t.string :email, null: false
+      t.string :subject, null: false
+      t.text :message, null: false
 
       t.timestamps
     end
+
+    add_index :contact_messages, :created_at
+    add_index :contact_messages, :email
   end
 end
