cc9c9beadff36a0c1ac36f3e13dc31b69759f031
Vítor Gabriel <108680269+vggd18@users.noreply.github.com>
2025-07-22 21:08:25 +0000

    feat: cors + routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
-  resources :contacts
-  resources :projects
-  resources :skills
+  resources :contacts, only: %i[index create]
+  resources :projects, except: %i[new edit]
+  resources :skills, only: %i[index create update destroy]
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
