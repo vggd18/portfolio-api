343194d6a7cca5c8a15e61a269b2e39c6ef08b1d
Vítor Gabriel <108680269+vggd18@users.noreply.github.com>
2025-07-22 21:06:52 +0000

    feat: environments cofigurations

--- Gemfile
+++ Gemfile
@@ -29,6 +29,10 @@ gem "kamal", require: false
 # Add HTTP asset caching/compression and X-Sendfile acceleration to Puma [https://github.com/basecamp/thruster/]
 gem "thruster", require: false
 
+# Cors
+gem 'rack-cors'
+
+
 # Use Active Storage variants [https://guides.rubyonrails.org/active_storage_overview.html#transforming-images]
 # gem "image_processing", "~> 1.2"
 
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,21 @@
 class ApplicationController < ActionController::API
+ before_action :set_cors_headers
+  
+  private
+  
+  def set_cors_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, PATCH, DELETE, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+  end
+  
+  def render_error(message, status = :unprocessable_entity)
+    render json: { error: message }, status: status
+  end
+  
+  def render_success(data, message = nil)
+    response = { data: data }
+    response[:message] = message if message
+    render json: response
+  end
 end
